a2776533c22cfb18dc9579e84b5d4a9d55d8f03b
Micro <armohamadgg12@gmail.com>
2026-06-29 21:59:44 +0400

    Update paramspider.py

--- Tools/Parametr_Automation/tools/paramspider.py
+++ Tools/Parametr_Automation/tools/paramspider.py
@@ -1,1 +1,28 @@
+import sys
+import subprocess
+from pathlib import Path
 
+
+def run_paramspider(target):
+    # Check paramspider is installed or isnt:
+    try:
+        subprocess.run(["paramspider", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        print("paramspider is not installed. Please install it first.")
+        sys.exit(1)
+
+    # Run paramspider:
+    try:
+        command = f"paramspider -d {target} -s | sort -u | httpx >> {target}_paramspider_output.txt"
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            print(f"Error running paramspider: {stderr.decode()}")
+            sys.exit(1)
+
+        return stdout.decode()
+    
+    except Exception as e:
+        print(f"error: {e}")
+        sys.exit(1)
