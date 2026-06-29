09a3970c696a22c50058740563d7772940e465b1
Micro <armohamadgg12@gmail.com>
2026-06-29 21:49:31 +0400

    main file

---
+++ Tools/Parametr_Automation/_main_.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+import time
+from tools.paramspider import run_paramspider
+from tools.qsreplace import run_qsreplace
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 2:
+        print(f'start with => python3 _main_.py domain')
+    else:    
+        input_target = sys.argv[1]
+        print(f'start tool {input_target}')
+
+    run_paramspider(input_target)
+    time.sleep(1.5)
+    run_qsreplace(input_target)
+    time.sleep(1.5)
+
+    
