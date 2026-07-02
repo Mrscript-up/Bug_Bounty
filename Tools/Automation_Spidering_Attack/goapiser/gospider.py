b5864eebf9882bf3ae4f9e3505c7cb67d10b7bbe
Micro <armohamadgg12@gmail.com>
2026-07-03 01:45:29 +0400

    Update gospider.py

--- Tools/Automation_Spidering_Attack/goapiser/gospider.py
+++ Tools/Automation_Spidering_Attack/goapiser/gospider.py
@@ -7,8 +7,8 @@ def start_gospider(domain):
     try:
         dom = f"{domain}"
         escaped = re.escape(dom)
-        run_cmd(f"gospider -s {domain} -d 2 -q | sort -u >> resoulve_gospider1.txt")
-        run_cmd(f'grep -E grep -E "^https?://([a-zA-Z0-9_-]+\.)*{escaped}(/|$)" resoulve_gospider1.txt | httpx >> /gospider/{domain}_resolve_gospider.txt')
+        run_cmd(f"gospider -s {domain} -d 2 -q | sort -u > resoulve_gospider1.txt")
+        run_cmd(f'grep -E grep -E "^https?://([a-zA-Z0-9_-]+\.)*{escaped}(/|$)" resoulve_gospider1.txt | httpx > /gospider/{domain}_resolve_gospider.txt')
         os.remove("resoulve_gospider1.txt")
     except:
         print('[~] Error...')
