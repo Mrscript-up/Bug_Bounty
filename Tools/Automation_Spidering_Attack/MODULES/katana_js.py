d73a2c38861f748828cdea2eaf253acd8187947d
Micro <armohamadgg12@gmail.com>
2026-07-02 23:17:01 +0400

    Update katana_js.py

--- Tools/Automation_Spidering_Attack/MODULES/katana_js.py
+++ Tools/Automation_Spidering_Attack/MODULES/katana_js.py
@@ -6,5 +6,5 @@ def katana_js(domain):
     dom = f"{domain}"
     escaped_dom = re.escape(dom)
     run_cmd(f"katana -u {domain} -jc -jsl -d 5 >> js.files_by_katana")
-    run_cmd(f'sort -u js.files_by_katana | grep -E "^https?://([^/]+\\.)?{escaped_dom}" | httpx >> js.files_by_httpx_katana1')
+    run_cmd(f'sort -u js.files_by_katana | grep -E "^https?://([^/]+\\.)?{escaped_dom}" | httpx > js.files_by_httpx_katana1')
     os.remove("js.files_by_katana")
