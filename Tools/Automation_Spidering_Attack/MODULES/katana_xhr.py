fd955ffe15db1212488f43ec802cba3881ec34fb
Micro <armohamadgg12@gmail.com>
2026-07-02 23:17:58 +0400

    Update katana_xhr.py

--- Tools/Automation_Spidering_Attack/MODULES/katana_xhr.py
+++ Tools/Automation_Spidering_Attack/MODULES/katana_xhr.py
@@ -5,5 +5,5 @@ def katana_xhr(domain):
     dom = domain
     dom2 = re.escape(dom)
     run_cmd(f"katana -u {domain} -xhr -d 5 -aff >> xhr_files_by_katana")
-    run_cmd(f'sort -u xhr_files_by_katana | grep -E "^https?://([^/]+\\.)?{dom2}" | httpx >> xhr_files_by_httpx_katana2')
+    run_cmd(f'sort -u xhr_files_by_katana | grep -E "^https?://([^/]+\\.)?{dom2}" | httpx > xhr_files_by_httpx_katana2')
     os.remove("xhr_files_by_katana")
