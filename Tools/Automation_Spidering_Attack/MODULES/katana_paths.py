8d00315c64a8186bb91f8ec5c2297f7a937fb02b
Micro <armohamadgg12@gmail.com>
2026-07-02 23:20:47 +0400

    Update katana_paths.py

--- Tools/Automation_Spidering_Attack/MODULES/katana_paths.py
+++ Tools/Automation_Spidering_Attack/MODULES/katana_paths.py
@@ -4,6 +4,6 @@ import re
 def katana_paths(domain):
     dom = domain
     dom2 = re.escape(dom)
-    run_cmd(f"katana {domain} -d 5 -fs fqdn >> paths_by_katana")
+    run_cmd(f"katana {domain} -d 5 -fs fqdn > paths_by_katana")
     run_cmd(f'sort -u paths_by_katana | grep -E "^https?://([^/]+\\.)?{dom2}" | httpx > paths_by_httpx_katana4')
     os.remove("paths_by_katana")
