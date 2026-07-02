4beae5262170532408683c609dc1d9353cf68229
Micro <armohamadgg12@gmail.com>
2026-07-02 23:15:33 +0400

    update

--- Tools/Automation_Spidering_Attack/MODULES/katana_standard.py
+++ Tools/Automation_Spidering_Attack/MODULES/katana_standard.py
@@ -1,7 +1,9 @@
 from MODULES.run_cmd import run_cmd
 import os
-
+import re
 def katana_standard(domain):
+    dom = domain
+    dom2 = re.escape(dom)
     run_cmd(f"katana -u {domain} -silent >> katana_standard")
-    run_cmd("cat katana_standard | sort -u | httpx >> katana_httpx_standard3")
+    run_cmd(f'sort -u katana_standard | grep -E "^https?://([^/]+\\.)?{dom2}" | httpx >> katana_httpx_standard3')
     os.remove("katana_standard")
