cacb7c733afd14550b5d7b2b6f28794c3b70dc47
Micro <armohamadgg12@gmail.com>
2026-07-03 01:43:28 +0400

    Update httpx.py

--- Tools/Automation_Spidering_Attack/parametr/tools/httpx.py
+++ Tools/Automation_Spidering_Attack/parametr/tools/httpx.py
@@ -5,7 +5,7 @@ from MODULES.run_cmd import run_cmd
 
 def run_httpx():
     # Define the command to run
-    command = f"httpx -l /paramspider/qs.output -content-length -silent -sc -hash mmh3 >> /paramspider/httpx_output.txt"
+    command = f"httpx -l /paramspider/qs.output -content-length -silent -sc -hash mmh3 > /paramspider/httpx_output.txt"
     
     # Run the command using subprocess
     try:
@@ -13,7 +13,7 @@ def run_httpx():
     except subprocess.CalledProcessError as e:
         print(f"[1] An error occurred while running httpx: {e}") 
     
-    command2 = f"httpx -l /paramspider/qs2_bb1b.output -content-length -silent -sc -hash mmh3 >> /paramspider/httpx_bb1b_output.txt"
+    command2 = f"httpx -l /paramspider/qs2_bb1b.output -content-length -silent -sc -hash mmh3 > /paramspider/httpx_bb1b_output.txt"
 
     try:
         run_cmd(command2)
